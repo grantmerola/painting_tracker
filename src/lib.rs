24cb8e66942e8bd3f0bf98350809b8715d31c44f
Grant Merola <grantmerola@gmail.com>
2019-05-08 04:01:31 -0700

    Figured out date usage + some diesel
    got dates working and got test to run with diesel

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,17 @@ extern crate diesel;
 use diesel::prelude::*;
 use diesel::mysql::MysqlConnection;
 mod schema;
-use diesel::sql_types::Date;
+//use diesel::sql_types::Date;
 use std::path::Path;
+
 extern crate chrono;
 use chrono::NaiveDate;
+
 
 // #[derive(Debug)]
 pub struct Work {
 	struct_work_name: String,
-	struct_creation_date: Date,
+	struct_creation_date: NaiveDate,
 	struct_location: String,
 	struct_bequeathment: String,
 	struct_source: String,
@@ -36,7 +38,7 @@ pub struct Work {
 
 impl Work {
 	pub fn new( struct_work_name: String,
-				struct_creation_date: Date,
+				struct_creation_date: NaiveDate,
 				struct_location: String,
 				struct_bequeathment: String,
 				struct_source: String,
@@ -69,7 +71,7 @@ impl Work {
 				.values(
 					(
 				        work_name.eq(&added_work.struct_work_name),
-				        // creation_date.eq(&added_work.struct_creation_date.FromSql()),
+				        creation_date.eq(&added_work.struct_creation_date),
 				        location.eq(&added_work.struct_location),
 				        bequeathment.eq(&added_work.struct_bequeathment),
 				        source.eq(&added_work.struct_source),
@@ -193,7 +195,7 @@ mod test {
 		str500: String,
 		str100: String,
 		str99: String,
-		adate: Date,
+		adate: NaiveDate,
 		work_name: String,
 		bequeathment: String,
 		source: String,
@@ -213,7 +215,7 @@ mod test {
 			str500: String::from("dkhgkhgkgkgkhgkhgkhgkhgkgkhgkgkhgkhgkhgkhgkgkhgkhgkhgkhgkhgkhgkhgkhgkgkhgkhgkhgkhgkhgkhgkhgkhgkgkhgkhgkhgkhgkhgkhgkhgkhgkhgkhgkhgkhgkhgkhgkgkhgkhgkhgkhgkhgkgkhgkhgkhgkhgkhgkhgkhgkhgkhgkhgkhgkgkgkhgkhgkhgkhgkgkhgkhgkhgkgkhgkhkhlghvfhfghjhdgfsghjkfgfdghjkhjghfdgrhfjgkhjlkljhgfdghjkldfghjkl;jhgfhjkljhgfdjgkhgkfjtgkhlukgyfjchkjghjkugjuyghjkilouyghjkiuyfghuiytghjuytrfghuyt6ryfhgjkiurtydfghjliuytfghjuytfhgbkuyutfgcvbhkygfhgkjuygjhfnbhmkyfghkugjhkuygtfhgkuytfhgkuytfgghjgytrdgfhjytfhgjkuiygtfhjkgfhghjyj"),
 			str100: String::from("sdkhgkhgkgkgkhgkhgkhgkhgkgkhgkgkhgkhgkhgkhgkgkhgkhgkhgkhgkhgkhgkhgkhgkgkhgkhgkhgkhgkhgkhgkhgkhgkgkhg"),
 			str99: String::from("dkhgkhgkgkgkhgkhgkhgkhgkgkhgkgkhgkhgkhgkhgkgkhgkhgkhgkhgkhgkhgkhgkhgkgkhgkhgkhgkhgkhgkhgkhgkhgkgkhg"),
-			adate: Date= NaiveDate::from_ymd(1996,3,13),
+			adate: NaiveDate::from_ymd(1996,3,13),
 			work_name: String::from("atitle"),
 			bequeathment: String::from("burned"),
 			source: String::from("Montana"),
@@ -228,58 +230,58 @@ mod test {
 			pool: make_new_pool()
 		}
 	}
-// 	#[test]
-// 	#[ignore]
-// 	fn new_location_test() {
-// 		let x = vars();
-// 		let should_work = Location::new(x.adate,x.location,x.comment,x.fk_works_id, &x.pool);
-// 	}
-// 	#[test]
-// 	#[should_panic]
-// 	#[ignore]
-// 	fn bad_fk_id_location_test() {
-// 		let x = vars();
-// 		let wont_work = Location::new(x.adate,x.location,x.comment,x.bad_fk_id, &x.pool);
-// 	}
-// 	#[test]
-// 	#[ignore]
-// 	fn location_test() {
-// 		let x = vars();
-// 		let wont_work = Location::new(x.adate,x.str499,x.comment,x.fk_works_id, &x.pool);
-// 	}
-// 	#[test]
-// 	#[should_panic]
-// 	#[ignore]
-// 	fn location_test_fail() {
-// 		let x = vars();
-// 		let wont_work = Location::new(x.adate,x.str500,x.comment,x.fk_works_id, &x.pool);
-// 	}
-// 	#[test]
-// 	#[ignore]
-// 	fn get_work_ids_test() {
-// 		let x = vars();
-// 		let should_work = get_work_ids(&x.pool);
-// 	}
-// 	#[test]
-// 	#[ignore]
-// 	fn make_new_pic_test() {
-// 		let x = vars();
-// 		let should_work = Pic::new(x.pic_path, x.quality, x.fk_works_id, &x.pool);
-// 	}
-// 	#[test]
-// 	#[should_panic]
-// 	#[ignore]
-// 	fn bad_pic_path_test() {
-// 		let x = vars();
-// 		let wont_work = Pic::new(x.bad_pic_path, x.quality, x.fk_works_id, &x.pool);
-// 	}
-// 	#[test]
-// 	#[should_panic]
-// 	#[ignore]
-// 	fn bad_fk_id_test() {
-// 		let x = vars();
-// 		let wont_work = Pic::new(x.pic_path, x.quality,x.bad_fk_id, &x.pool);
-// 	}
+	#[test]
+	#[ignore]
+	fn new_location_test() {
+		let x = vars();
+		let should_work = Location::new(x.adate,x.location,x.comment,x.fk_works_id, &x.pool);
+	}
+	#[test]
+	#[should_panic]
+	#[ignore]
+	fn bad_fk_id_location_test() {
+		let x = vars();
+		let wont_work = Location::new(x.adate,x.location,x.comment,x.bad_fk_id, &x.pool);
+	}
+	#[test]
+	#[ignore]
+	fn location_test() {
+		let x = vars();
+		let wont_work = Location::new(x.adate,x.str499,x.comment,x.fk_works_id, &x.pool);
+	}
+	#[test]
+	#[should_panic]
+	#[ignore]
+	fn location_test_fail() {
+		let x = vars();
+		let wont_work = Location::new(x.adate,x.str500,x.comment,x.fk_works_id, &x.pool);
+	}
+	#[test]
+	#[ignore]
+	fn get_work_ids_test() {
+		let x = vars();
+		let should_work = get_work_ids(&x.pool);
+	}
+	#[test]
+	#[ignore]
+	fn make_new_pic_test() {
+		let x = vars();
+		let should_work = Pic::new(x.pic_path, x.quality, x.fk_works_id, &x.pool);
+	}
+	#[test]
+	#[should_panic]
+	#[ignore]
+	fn bad_pic_path_test() {
+		let x = vars();
+		let wont_work = Pic::new(x.bad_pic_path, x.quality, x.fk_works_id, &x.pool);
+	}
+	#[test]
+	#[should_panic]
+	#[ignore]
+	fn bad_fk_id_test() {
+		let x = vars();
+		let wont_work = Pic::new(x.pic_path, x.quality,x.bad_fk_id, &x.pool);
+	}
 	#[test]
 	#[ignore]
 	fn make_new_work_test() {
@@ -287,56 +289,56 @@ mod test {
 		let should_work = Work::new(x.work_name,x.adate,x.location,x.bequeathment,x.source,x.medium,x.comment,&x.pool);
 	}
 	
-// 	#[test]
-// 	#[ignore]
-// 	fn work_name_test(){
-// 		let x = vars();
-// 		let should_work = Work::new(x.str499,x.adate,x.location,x.bequeathment,x.source,x.medium,x.comment,&x.pool);
-// 	}
-// 	#[test]
-// 	#[should_panic]
-// 	#[ignore]
-// 	fn work_name_test_fail(){
-// 		let x = vars();
-// 		let wont_work = Work::new(x.str500,x.adate,x.location,x.bequeathment,x.source,x.medium,x.comment,&x.pool);
-// 	}
-// 	#[test]	
-// 	#[ignore]
-// 	fn bequeth_test() {
-// 		let x = vars();
-// 		let should_work = Work::new(x.work_name,x.adate,x.location,x.str499,x.source,x.medium,x.comment,&x.pool);
-// 	}
-// 	#[test]
-// 	#[should_panic]
-// 	#[ignore]
-// 	fn bequeth_test_fail() {
-// 		let x = vars();
-// 		let wont_work = Work::new(x.work_name,x.adate,x.location,x.str500,x.source,x.medium,x.comment,&x.pool);
-// 	}
-// 	#[test]	
-// 	#[ignore]
-// 	fn source_test() {
-// 		let x = vars();
-// 		let should_work = Work::new(x.work_name,x.adate,x.location,x.bequeathment,x.str499,x.medium,x.comment,&x.pool);
-// 	}
-// 	#[test]
-// 	#[should_panic]
-// 	#[ignore]
-// 	fn source_test_fail() {
-// 		let x = vars();
-// 		let wont_work = Work::new(x.work_name,x.adate,x.location,x.bequeathment,x.str500,x.medium,x.comment,&x.pool);
-// 	}
-// 	#[test]	
-// 	#[ignore]
-// 	fn medium_test() {
-// 		let x = vars();
-// 		let should_work = Work::new(x.work_name,x.adate,x.location,x.bequeathment,x.source,x.str99,x.comment,&x.pool);
-// 	}
-// 	#[test]
-// 	#[should_panic]
-// 	#[ignore]
-// 	fn medium_test_fail() {
-// 		let x = vars();
-// 		let wont_work = Work::new(x.work_name,x.adate,x.location,x.bequeathment,x.source,x.str100,x.comment,&x.pool);
-// 	}
+	#[test]
+	#[ignore]
+	fn work_name_test(){
+		let x = vars();
+		let should_work = Work::new(x.str499,x.adate,x.location,x.bequeathment,x.source,x.medium,x.comment,&x.pool);
+	}
+	#[test]
+	#[should_panic]
+	#[ignore]
+	fn work_name_test_fail(){
+		let x = vars();
+		let wont_work = Work::new(x.str500,x.adate,x.location,x.bequeathment,x.source,x.medium,x.comment,&x.pool);
+	}
+	#[test]	
+	#[ignore]
+	fn bequeth_test() {
+		let x = vars();
+		let should_work = Work::new(x.work_name,x.adate,x.location,x.str499,x.source,x.medium,x.comment,&x.pool);
+	}
+	#[test]
+	#[should_panic]
+	#[ignore]
+	fn bequeth_test_fail() {
+		let x = vars();
+		let wont_work = Work::new(x.work_name,x.adate,x.location,x.str500,x.source,x.medium,x.comment,&x.pool);
+	}
+	#[test]	
+	#[ignore]
+	fn source_test() {
+		let x = vars();
+		let should_work = Work::new(x.work_name,x.adate,x.location,x.bequeathment,x.str499,x.medium,x.comment,&x.pool);
+	}
+	#[test]
+	#[should_panic]
+	#[ignore]
+	fn source_test_fail() {
+		let x = vars();
+		let wont_work = Work::new(x.work_name,x.adate,x.location,x.bequeathment,x.str500,x.medium,x.comment,&x.pool);
+	}
+	#[test]	
+	#[ignore]
+	fn medium_test() {
+		let x = vars();
+		let should_work = Work::new(x.work_name,x.adate,x.location,x.bequeathment,x.source,x.str99,x.comment,&x.pool);
+	}
+	#[test]
+	#[should_panic]
+	#[ignore]
+	fn medium_test_fail() {
+		let x = vars();
+		let wont_work = Work::new(x.work_name,x.adate,x.location,x.bequeathment,x.source,x.str100,x.comment,&x.pool);
+	}
 }
